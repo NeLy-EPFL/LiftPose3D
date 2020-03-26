65f063c6db86408d66ec7c4a9f9a590ff80f5127
Adam Gosztolai <a.gosztolai@gmail.com>
2020-03-26 12:02:40 +0100

    plots

--- make_video_of_prediction.py
+++ make_video_of_prediction.py
@@ -13,20 +13,29 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from matplotlib.animation import FFMpegWriter
 
-def plot_fly(graph, pos, ax=None):
-            
-#    nx.draw_networkx_nodes(graph, pos = pos, node_size = 10, ax=ax)
-#    nx.draw_networkx_edges(graph, pos = pos, width = 1, edge_color = '0.5', ax=ax)
+def plot_fly(graph, pos_pred, pos_tar, ax=None):
     
-    pos = np.array(pos)    
-    ax.scatter(pos[:,0], pos[:,1], pos[:,2], c='k', s=10, edgecolors='k', alpha=0.7)
+    ax.cla()
+    
+    pos_pred = np.array(pos_pred)    
+    ax.scatter(-pos_pred[:,0], -pos_pred[:,1], -pos_pred[:,2], c='b', s=10, edgecolors='k', alpha=0.7)
            
     for _, j in enumerate(G.edges()): 
-        x = np.array((pos[j[0]][0], pos[j[1]][0]))
-        y = np.array((pos[j[0]][1], pos[j[1]][1]))
-        z = np.array((pos[j[0]][2], pos[j[1]][2]))
+        x = np.array((-pos_pred[j[0]][0], -pos_pred[j[1]][0]))
+        y = np.array((-pos_pred[j[0]][1], -pos_pred[j[1]][1]))
+        z = np.array((-pos_pred[j[0]][2], -pos_pred[j[1]][2]))
                    
-        ax.plot(x, y, z, c='r', alpha=0.3, linewidth = 1)
+        ax.plot(x, y, z, c='b', alpha=0.3, linewidth = 1)
+        
+    pos_tar = np.array(pos_tar)    
+    ax.scatter(-pos_tar[:,0], -pos_tar[:,1], -pos_tar[:,2], c='r', s=10, edgecolors='k', alpha=0.7)
+           
+    for _, j in enumerate(G.edges()): 
+        x = np.array((-pos_tar[j[0]][0], -pos_tar[j[1]][0]))
+        y = np.array((-pos_tar[j[0]][1], -pos_tar[j[1]][1]))
+        z = np.array((-pos_tar[j[0]][2], -pos_tar[j[1]][2]))
+                   
+        ax.plot(x, y, z, c='r', alpha=0.3, linewidth = 1)    
 
     ax.view_init(elev = 10, azim=290)
 
@@ -69,36 +78,40 @@ edges = [(0,1),(1,2),(2,3),(3,4),(5,6),(6,7),(7,8),(8,9),(10,11),(11,12),(12,13)
 edges = [e for e in edges if e[0]<15 and e[1]<15]
 nodes = list(range(15))
 
+anchors = [0, 5, 10]
+target_sets = [[1, 2, 3, 4], [6, 7, 8, 9], [11, 12, 13, 14]]
+
+#build graph
 G=nx.Graph()
 G.add_edges_from(edges)
 G.add_nodes_from(nodes)
 
+#load data and prediction
 data = torch.load('checkpoint/test/MDN_CsCh_test.pth.tar')
-
 output = data['output']
 target = data['target']
 
 #put back the anchor points
-anchors = [0, 5, 10]
-target_sets = [[1, 2, 3, 4], [6, 7, 8, 9], [11, 12, 13, 14]]
-
 output_full = np.zeros((output.shape[0], 3*15))
 target_full = np.zeros((target.shape[0], 3*15))
-
 for i, anch in enumerate(anchors):
     for j in target_sets[i]:            
         output_full[:, 3*j:3*j+3] = output[:, 3*(j-i-1):3*(j-i-1)+3]
+        target_full[:, 3*j:3*j+3] = target[:, 3*(j-i-1):3*(j-i-1)+3]
 
+#plot
 fig = plt.figure(figsize=(5,4))
 ax = fig.add_subplot(111, projection = '3d')
 
 metadata = dict(title='LiftFly3D prediction', artist='Nely',comment='Watch this!')
-writer = FFMpegWriter(fps=1, metadata=metadata)
+writer = FFMpegWriter(fps=10, metadata=metadata)
 with writer.saving(fig, "LiftFly3d_prediction.mp4", 100):
     for t in range(100):
-        pos = []
+        pos_pred = []
+        pos_tar = []
         for j in range(int(output_full.shape[1]/3)):
-            pos.append((output_full[t, 3*j], output_full[t, 3*j+1], output_full[t, 3*j+2]))
+            pos_pred.append((output_full[t, 3*j], output_full[t, 3*j+1], output_full[t, 3*j+2]))
+            pos_tar.append((target_full[t, 3*j], target_full[t, 3*j+1], target_full[t, 3*j+2]))
             
-        ax = plot_fly(G, pos, ax=ax)
+        ax = plot_fly(G, pos_pred, pos_tar, ax=ax)
         writer.grab_frame()
