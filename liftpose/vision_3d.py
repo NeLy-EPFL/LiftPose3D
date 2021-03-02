350117c716d06916f71da92811a45ddbfa4242b2
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2021-03-02 09:20:49 +0100

    capture results

--- liftpose/vision_3d.py
+++ liftpose/vision_3d.py
@@ -398,116 +398,116 @@ def intrinsic_matrix(fx, fy, cx, cy):
     return intr
 
 
-def procrustes(X, Y, scaling=True, reflection="best"):
-    """
-    A port of MATLAB's `procrustes` function to Numpy.
+# def procrustes(X, Y, scaling=True, reflection="best"):
+#     """
+#     A port of MATLAB's `procrustes` function to Numpy.
 
-    Procrustes analysis determines a linear transformation (translation,
-    reflection, orthogonal rotation and scaling) of the points in Y to best
-    conform them to the points in matrix X, using the sum of squared errors
-    as the goodness of fit criterion.
+#     Procrustes analysis determines a linear transformation (translation,
+#     reflection, orthogonal rotation and scaling) of the points in Y to best
+#     conform them to the points in matrix X, using the sum of squared errors
+#     as the goodness of fit criterion.
 
-        d, Z, [tform] = procrustes(X, Y)
+#         d, Z, [tform] = procrustes(X, Y)
 
-    Inputs:
-    ------------
-    X, Y    
-        matrices of target and input coordinates. they must have equal
-        numbers of  points (rows), but Y may have fewer dimensions
-        (columns) than X.
+#     Inputs:
+#     ------------
+#     X, Y    
+#         matrices of target and input coordinates. they must have equal
+#         numbers of  points (rows), but Y may have fewer dimensions
+#         (columns) than X.
 
-    scaling 
-        if False, the scaling component of the transformation is forced
-        to 1
+#     scaling 
+#         if False, the scaling component of the transformation is forced
+#         to 1
 
-    reflection
-        if 'best' (default), the transformation solution may or may not
-        include a reflection component, depending on which fits the data
-        best. setting reflection to True or False forces a solution with
-        reflection or no reflection respectively.
+#     reflection
+#         if 'best' (default), the transformation solution may or may not
+#         include a reflection component, depending on which fits the data
+#         best. setting reflection to True or False forces a solution with
+#         reflection or no reflection respectively.
 
-    Outputs
-    ------------
-    d       
-        the residual sum of squared errors, normalized according to a
-        measure of the scale of X, ((X - X.mean(0))**2).sum()
+#     Outputs
+#     ------------
+#     d       
+#         the residual sum of squared errors, normalized according to a
+#         measure of the scale of X, ((X - X.mean(0))**2).sum()
 
-    Z
-        the matrix of transformed Y-values
+#     Z
+#         the matrix of transformed Y-values
 
-    tform   
-        a dict specifying the rotation, translation and scaling that
-        maps X --> Y
+#     tform   
+#         a dict specifying the rotation, translation and scaling that
+#         maps X --> Y
 
-    """
+#     """
 
-    n, m = X.shape
-    ny, my = Y.shape
+#     n, m = X.shape
+#     ny, my = Y.shape
 
-    muX = X.mean(0)
-    muY = Y.mean(0)
+#     muX = X.mean(0)
+#     muY = Y.mean(0)
 
-    X0 = X - muX
-    Y0 = Y - muY
+#     X0 = X - muX
+#     Y0 = Y - muY
 
-    ssX = (X0 ** 2.0).sum()
-    ssY = (Y0 ** 2.0).sum()
+#     ssX = (X0 ** 2.0).sum()
+#     ssY = (Y0 ** 2.0).sum()
 
-    # centred Frobenius norm
-    normX = np.sqrt(ssX)
-    normY = np.sqrt(ssY)
+#     # centred Frobenius norm
+#     normX = np.sqrt(ssX)
+#     normY = np.sqrt(ssY)
 
-    # scale to equal (unit) norm
-    X0 /= normX
-    Y0 /= normY
+#     # scale to equal (unit) norm
+#     X0 /= normX
+#     Y0 /= normY
 
-    if my < m:
-        Y0 = np.concatenate((Y0, np.zeros(n, m - my)), 0)
+#     if my < m:
+#         Y0 = np.concatenate((Y0, np.zeros(n, m - my)), 0)
 
-    # optimum rotation matrix of Y
-    A = np.dot(X0.T, Y0)
-    U, s, Vt = np.linalg.svd(A, full_matrices=False)
-    V = Vt.T
-    T = np.dot(V, U.T)
+#     # optimum rotation matrix of Y
+#     A = np.dot(X0.T, Y0)
+#     U, s, Vt = np.linalg.svd(A, full_matrices=False)
+#     V = Vt.T
+#     T = np.dot(V, U.T)
 
-    if reflection != "best":
+#     if reflection != "best":
 
-        # does the current solution use a reflection?
-        have_reflection = np.linalg.det(T) < 0
+#         # does the current solution use a reflection?
+#         have_reflection = np.linalg.det(T) < 0
 
-        # if that's not what was specified, force another reflection
-        if reflection != have_reflection:
-            V[:, -1] *= -1
-            s[-1] *= -1
-            T = np.dot(V, U.T)
+#         # if that's not what was specified, force another reflection
+#         if reflection != have_reflection:
+#             V[:, -1] *= -1
+#             s[-1] *= -1
+#             T = np.dot(V, U.T)
 
-    traceTA = s.sum()
+#     traceTA = s.sum()
 
-    if scaling:
+#     if scaling:
 
-        # optimum scaling of Y
-        b = traceTA * normX / normY
+#         # optimum scaling of Y
+#         b = traceTA * normX / normY
 
-        # standarised distance between X and b*Y*T + c
-        d = 1 - traceTA ** 2
+#         # standarised distance between X and b*Y*T + c
+#         d = 1 - traceTA ** 2
 
-        # transformed coords
-        Z = normX * traceTA * np.dot(Y0, T) + muX
+#         # transformed coords
+#         Z = normX * traceTA * np.dot(Y0, T) + muX
 
-    else:
-        b = 1
-        d = 1 + ssY / ssX - 2 * traceTA * normY / normX
-        Z = normY * np.dot(Y0, T) + muX
+#     else:
+#         b = 1
+#         d = 1 + ssY / ssX - 2 * traceTA * normY / normX
+#         Z = normY * np.dot(Y0, T) + muX
 
-    # transformation matrix
-    if my < m:
-        T = T[:my, :]
-    c = muX - b * np.dot(muY, T)
+#     # transformation matrix
+#     if my < m:
+#         T = T[:my, :]
+#     c = muX - b * np.dot(muY, T)
 
-    # transformation values
-    tform = {"rotation": T, "scale": b, "translation": c}
+#     # transformation values
+#     tform = {"rotation": T, "scale": b, "translation": c}
 
-    return d, Z, tform
+#     return d, Z, tform
 
 
 def find_neighbours(k, pts, target_pts, nn):
