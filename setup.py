8792f7873b7cf23892390e149e6c254019019b7c
Semih Gunel <gunelsemih@gmail.com>
2021-03-03 15:01:29 +0100

    making invariant work

--- setup.py
+++ setup.py
@@ -11,13 +11,5 @@ setuptools.setup(
     description="Monocular 3D pose Estimation on Labatoary Animals",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/NeLy-EPFL/LiftPose3D",
-    install_requires=[
-        "scipy==1.2.0", # from scipy.transform import Rot
-        "numpy",
-        "networkx",
-        "matplotlib",
-        "torchvision",
-        "tqdm",
-    ],
+    url="https://github.com/NeLy-EPFL/LiftPose3D"
 )
