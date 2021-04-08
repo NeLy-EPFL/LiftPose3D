194e9529150053479a32119ad2c8c94b01824e09
semihgunel <gunelsemih@gmail.com>
2021-04-08 16:20:56 +0200

    fix typo on desc; increase version

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="liftpose",
-    version="0.1",
+    version="0.22",
     author="Adam Gosztolai & Semih Gunel",
     packages=["liftpose"],
-    description="Monocular 3D pose Estimation on Labatoary Animals",
+    description="Monocular 3D pose Estimation on Laboratory Animals",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/NeLy-EPFL/LiftPose3D"
